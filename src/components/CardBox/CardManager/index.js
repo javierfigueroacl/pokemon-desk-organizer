23ed62ee43d5c9aeb11c33d31cc1a6aa44259362
Javier Figueroa <javierfigueroacl@gmail.com>
2023-09-29 11:47:58 -0700

    Componetize Card Management logic

---
+++ src/components/CardBox/CardManager/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+import { red } from "../../../helpers/colors";
+import { useCards, useCardsDispatch } from "../../../cardContext";
+import { saveCard, clearCards } from "../../../state/actions/cardsActions";
+import Column from "../Column";
+import CardInfo from "../CardInfo";
+
+const ClearButton = styled.button`
+  grid-area: clear-saved-cards;
+  background-color: ${red};
+  color: white;
+  font-weight: bold;
+  border-radius: 1rem;
+  width: 20%;
+  min-width: 15rem;
+  margin: auto;
+  height: 2rem;
+  cursor: pointer;
+`;
+
+const CardManager = () => {
+  const { cards, savedCards, selectedCard } = useCards();
+  const dispatch = useCardsDispatch();
+
+  /* This is the callback after drag. In this case we only perform actions
+  if the source column is different from the destination column, but the
+  library supports all cases. */
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+
+    if (!destination) return;
+    dispatch(saveCard({ source, destination }));
+  };
+
+  const handleClick = () => dispatch(clearCards());
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Column cards={cards} id="cards" />
+        <Column cards={savedCards} id="savedCards" />
+      </DragDropContext>
+      {savedCards.length > 0 && (
+        <ClearButton onClick={handleClick}>Clear Saved Cards List</ClearButton>
+      )}
+      <CardInfo selectedCard={selectedCard} />
+    </>
+  );
+};
+
+export default CardManager;
--- src/components/CardBox/index.js
+++ src/components/CardBox/index.js
@@ -1,27 +1,9 @@
 import React from "react";
-
-import { DragDropContext } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-import { useCards, useCardsDispatch } from "../../cardContext";
-import { saveCard, clearCards } from "../../state/actions/cardsActions";
-import { red, blue } from "../../helpers/colors";
-import Column from "./Column";
-import CardInfo from "./CardInfo";
+import { blue } from "../../helpers/colors";
 import CardSearch from "./CardSearch";
-
-const ClearButton = styled.button`
-  grid-area: clear-saved-cards;
-  background-color: ${red};
-  color: white;
-  font-weight: bold;
-  border-radius: 1rem;
-  width: 20%;
-  min-width: 15rem;
-  margin: auto;
-  height: 2rem;
-  cursor: pointer;
-`;
+import CardManager from "./CardManager";
 
 const Instructions = styled.div`
   grid-area: instructions;
@@ -32,21 +14,6 @@ const Instructions = styled.div`
 // DragDropContext is a component of the react-beautiful-dnd library, all
 // drag and drop actions can only occur within it.
 const CardBox = () => {
-  const { cards, savedCards, selectedCard } = useCards();
-  const dispatch = useCardsDispatch();
-
-  /* This is the callback after drag. In this case we only perform actions
-  if the source column is different from the destination column, but the
-  library supports all cases. */
-  const onDragEnd = (result) => {
-    const { destination, source } = result;
-
-    if (!destination) return;
-    dispatch(saveCard({ source, destination }));
-  };
-
-  const handleClick = () => dispatch(clearCards());
-
   return (
     <>
       <CardSearch />
@@ -54,14 +21,7 @@ const CardBox = () => {
         Move cards to the right column to save them. You can delete them by
         returning them to the left column.
       </Instructions>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Column cards={cards} id="cards" />
-        <Column cards={savedCards} id="savedCards" />
-      </DragDropContext>
-      {savedCards.length > 0 && (
-        <ClearButton onClick={handleClick}>Clear Saved Cards List</ClearButton>
-      )}
-      <CardInfo selectedCard={selectedCard} />
+      <CardManager />
     </>
   );
 };
